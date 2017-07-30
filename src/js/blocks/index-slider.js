daaaba6b713bd5c4f0f1fbcf93a7161980885031
Yuriy <ketov1992@list.ru>
2017-07-30 18:15:22 +0500

    first commit cover.html

---
+++ src/js/blocks/index-slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var swiper = new Swiper('.index-slider', {
+        autoplay: 7000,
+        autoplayDisableOnInteraction: false,
+        paginationClickable: true,
+        loop: true,
+        speed: 550,
+    });
+});
