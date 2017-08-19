6df1ce633b7a53f7d567b1791b7f2befa1d2c439
Yuriy <ketov1992@gmail.com>
2017-08-19 14:45:55 +0500

    mobile

--- src/js/blocks/header.js
+++ src/js/blocks/header.js
@@ -4,12 +4,14 @@ $(document).ready(function () {
         var p = el.parent('.in-wrapper');
         if (el.hasClass('active')) {
             p.children('nav').stop(true, true).slideUp(250);
-            p.children('.contacts').stop(true, true).slideUp(250);
+            //p.children('.contacts').stop(true, true).slideUp(250);
             el.removeClass('active');
+            $('header').removeClass('active');
         } else {
             p.children('nav').stop(true, true).slideDown(250);
-            p.children('.contacts').stop(true, true).slideDown(250);
+            //p.children('.contacts').stop(true, true).slideDown(250);
             el.addClass('active');
+            $('header').addClass('active');
         }
         // $('#black-bg').stop(true, true).fadeIn(400);
         //setTimeout(function () {
